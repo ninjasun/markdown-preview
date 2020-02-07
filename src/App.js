528037c35bdc8bd217334f92fa9fd6b38ddfaafa
ninjafat <davide.mezzetti@hotmail.it>
2020-02-07 17:32:29 +0100

    completed user story 4 and add some style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+// Create reference instance
+import marked from'marked';
 
 import './App.css';
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+const initialContent = '# Welcome to my React Markdown Previewer!';
 
 class App extends React.Component {
 
@@ -11,13 +25,21 @@ class App extends React.Component {
       previewContent: ''
     }
     this.onChange = this.onChange.bind(this);
+ 
+  }
+
+  componentDidMount(){
+    this.setState({
+      content: initialContent
+    })
   }
 
   onChange(e){
-    console.log("onChange: ", e)
+    console.log("onChange: ", e.target.value);
+    let mar = marked(e.target.value);
     this.setState({
       content: e.target.value,
-      previewContent: e.target.value
+      previewContent: mar
     })
   }
 
@@ -25,11 +47,16 @@ class App extends React.Component {
     const { content, previewContent } = this.state;
     return (
       <div className="App">
-        <textarea id="editor" onChange={this.onChange} value={content}/>
-          
-        <div id="preview">
-          {previewContent}
+        <div className="editor-container">
+          <div className="editor-header">
+             <p className="editor-title">Editor</p>
+          </div>
+          <textarea id="editor" onChange={this.onChange} value={content}/>
         </div>
+        
+          
+        <div id="preview" dangerouslySetInnerHTML={{ __html: previewContent }} />
+
       </div>
     );
   }
